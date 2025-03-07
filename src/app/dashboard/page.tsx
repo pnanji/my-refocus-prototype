8c77727475666e5cf2722bfb3bda6bcc1b7a592c
Paymon Nanji <paymonnanji@icloud.com>
2025-03-06 20:49:22 -0800

    first updates

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { DashboardLayout } from "@/components/layout/dashboard-layout";
 import {
-  Breadcrumb,
+  BreadcrumbList as Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
